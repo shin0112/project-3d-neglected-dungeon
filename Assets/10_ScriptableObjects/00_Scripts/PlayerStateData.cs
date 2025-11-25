bdb915331f6b5b788bb4b3d7ff583451514f0ccb
shin0112 <wnslcosltimo12@gmail.com>
2025-11-25 12:21:44 +0900

    feat: 플레이어 state 관리를 위한 SO 만들기

---
+++ Assets/10_ScriptableObjects/00_Scripts/PlayerStateData.cs
@@ -0,0 +1,34 @@
+using System;
+using UnityEngine;
+
+/// <summary>
+/// 플레이어의 상태 값을 저장하기 위한 SO
+/// </summary>
+[Serializable]
+public class PlayerGroundData
+{
+    [field: SerializeField][field: Range(0f, 25f)] public float BaseSpeed { get; private set; } = 5f;
+    [field: SerializeField][field: Range(0f, 25f)] public float BaseRotationDamping { get; private set; } = 1f;
+
+    [field: Header("IdleData")]
+
+    [field: Header("WalkData")]
+    [field: SerializeField][field: Range(0f, 2f)] public float WalkSpeedModifier { get; private set; } = 0.225f;
+
+    [field: Header("RunData")]
+    [field: SerializeField][field: Range(0f, 2f)] public float RunSpeedModifier { get; private set; } = 1f;
+}
+
+[Serializable]
+public class PlayerAirData
+{
+    [field: Header("JumpData")]
+    [field: SerializeField][field: Range(0f, 25f)] public float JumpForce { get; private set; } = 5f;
+}
+
+[CreateAssetMenu(fileName = "PlayerState", menuName = "Entites/Player")]
+public class PlayerStateData : ScriptableObject
+{
+    [field: SerializeField] public PlayerGroundData GroundData { get; private set; }
+    [field: SerializeField] public PlayerAirData AirData { get; private set; }
+}
