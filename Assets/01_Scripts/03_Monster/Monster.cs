ccae3eafc01721e40b0575cde428752412f796c8
shin0112 <wnslcosltimo12@gmail.com>
2025-11-26 19:26:10 +0900

    feat: Monster IPoolable 구현하기

--- Assets/01_Scripts/03_Monster/Monster.cs
+++ Assets/01_Scripts/03_Monster/Monster.cs
@@ -1,18 +1,23 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class Monster : MonoBehaviour, IAttackable
+public abstract class Monster : MonoBehaviour, IAttackable, IPoolable
 {
     [field: Header("Data")]
-    [field: SerializeField] public MonsterStatData Stat { get; private set; }
+    [field: SerializeField] public MonsterData Data { get; private set; }
     public Dictionary<StatType, float> StatDict { get; private set; }
-    private float _hp;
-    public bool IsAlive => _hp > 0;
 
     [field: Header("Animation")]
     [field: SerializeField] public Animator Animator { get; private set; }
     [field: SerializeField] public MonsterAnimationData AnimationData { get; private set; }
     protected MonsterStateMachine monsterStateMachine;
+
+    private float _hp;
+    private event Action<GameObject> _returnAction;
+    public bool IsAlive => _hp > 0;
+
+    public event Action<Monster> OnDead;
 
     // IAttackable
     public Transform Transform => transform;
@@ -49,7 +54,7 @@ public abstract class Monster : MonoBehaviour, IAttackable
     {
         StatDict = new();
 
-        foreach (StatEntry statEntry in Stat.Stats)
+        foreach (StatEntry statEntry in Data.Stat.Stats)
         {
             StatDict[statEntry.StatType] = statEntry.BaseValue;
         }
@@ -72,7 +77,25 @@ public abstract class Monster : MonoBehaviour, IAttackable
     private void Die()
     {
         Logger.Log("몬스터 사망");
-        // todo: 오브젝트 풀링으로 관리 or destroy
+        OnDead?.Invoke(this);
+        ReturnToPool();
+    }
+    #endregion
+
+    #region IPoolable 구현
+    public void Initialize(Action<GameObject> returnAction)
+    {
+        _returnAction = returnAction;
+    }
+
+    public void OnSpawn()
+    {
+        _hp = StatDict[StatType.Health];
+    }
+
+    public void ReturnToPool()
+    {
+        _returnAction?.Invoke(gameObject);
     }
     #endregion
 }
