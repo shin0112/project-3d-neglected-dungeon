37799871ef95e4eb20e36df4835346762cafc4c6
shin0112 <wnslcosltimo12@gmail.com>
2025-11-26 19:25:43 +0900

    rename: IPoolable 메서드 이름 변경

--- Assets/01_Scripts/01_Common/IPoolable.cs
+++ Assets/01_Scripts/01_Common/IPoolable.cs
@@ -5,5 +5,5 @@ public interface IPoolable
 {
     public void Initialize(Action<GameObject> returnAction);
     public void OnSpawn();
-    public void OnDespwan();
+    public void ReturnToPool();
 }
