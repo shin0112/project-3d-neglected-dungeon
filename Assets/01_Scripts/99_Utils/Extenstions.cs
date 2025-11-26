8974cd6214bf0024b5ef27aab263263d64f3e36c
shin0112 <wnslcosltimo12@gmail.com>
2025-11-26 23:37:09 +0900

    feat: array에 random 메서드 확장하기

--- Assets/01_Scripts/99_Utils/Extenstions.cs
+++ Assets/01_Scripts/99_Utils/Extenstions.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 /// </summary>
 
 #region Transform
-public static class TransformExtenstion
+public static class TransformExtension
 {
     public static T FindChild<T>(this Transform t, string name) where T : Component
     {
@@ -21,3 +21,16 @@ public static class TransformExtenstion
     }
 }
 #endregion
+
+public static class ArrayExtension
+{
+    private static readonly System.Random random = new System.Random();
+
+    public static T Random<T>(this T[] array)
+    {
+        if (array == null || array.Length == 0)
+            return default;
+
+        return array[random.Next(array.Length)];
+    }
+}
