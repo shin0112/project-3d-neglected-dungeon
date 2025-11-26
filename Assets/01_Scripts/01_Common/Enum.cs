d2012338b394a202f25e69632ec26866dd27a591
shin0112 <wnslcosltimo12@gmail.com>
2025-11-26 14:43:02 +0900

    refactor: enum 파일 생성 (한 번에 모아서 관리), Define.cs common 폴더로 이동

---
+++ Assets/01_Scripts/01_Common/Enum.cs
@@ -0,0 +1,63 @@
+#region 스텟
+public enum StatType
+{
+    Health,
+    Stamina,
+    AttackDistance,
+    DetectDistance,
+}
+#endregion
+
+#region 던전
+public enum BossType
+{
+
+}
+#endregion
+
+#region 아이템
+// 아이템 타입
+public enum ItemType
+{
+    Equipment,
+    Consumable,
+}
+
+// 아이템 등급
+public enum ItemClass
+{
+    Normal,
+    Rare,
+    Elite,
+    Unique
+}
+
+// 장비 아이템 타입
+public enum EquipmentType
+{
+    Weapon,
+    Armor,
+}
+
+// 장비 아이템 착용 시 적용 스텟
+public enum EquipmentStats
+{
+    Attack,
+    SkillAttack,
+    Health
+}
+
+// 소비 아이템 타입
+public enum ConsumableType
+{
+    Portion,
+    Buff,
+}
+
+// 소비 아이템 사용 시 적용 스텟
+public enum ConsumableStats
+{
+    Health,
+    Stamina,
+}
+#endregion
--- Assets/10_ScriptableObjects/00_Scripts/Stat/StatData.cs
+++ Assets/10_ScriptableObjects/00_Scripts/Stat/StatData.cs
@@ -3,14 +3,6 @@ using UnityEngine;
 /// <summary>
 /// Entity의 스탯과 값을 저장하기 위한 클래스
 /// </summary>
-public enum StatType
-{
-    Health,
-    Stamina,
-    AttackDistance,
-    DetectDistance,
-}
-
 [System.Serializable]
 public class StatEntry
 {
