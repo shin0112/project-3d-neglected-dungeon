9273fb02d2a36590c3eb12ad9beba1ec2f623bf9
shin0112 <wnslcosltimo12@gmail.com>
2025-11-26 10:40:12 +0900

    feat: 몬스터 클래스 추상 클래스로 변환, 애니메이터, 상태 머신과 연결하기

--- Assets/01_Scripts/03_Monster/Monster.cs
+++ Assets/01_Scripts/03_Monster/Monster.cs
@@ -1,6 +1,27 @@
 using UnityEngine;
 
-public class Monster : MonoBehaviour
+public abstract class Monster : MonoBehaviour
 {
+    [field: Header("Animations")]
+    [field: SerializeField] public Animator Animator { get; private set; }
+    [field: SerializeField] public MonsterAnimationData AnimationData { get; private set; }
+    protected MonsterStateMachine monsterStateMachine;
+
     [field: SerializeField] public bool IsAlive { get; private set; } = true;
+
+    #region 초기화
+    protected void Awake()
+    {
+        GetComponents();
+
+        AnimationData.Initialize();
+        this.monsterStateMachine = new(this);
+        this.monsterStateMachine.ChangeState(monsterStateMachine.IdleState);
+    }
+
+    private void GetComponents()
+    {
+        Animator = GetComponent<Animator>();
+    }
+    #endregion
 }
