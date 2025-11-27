8012b613758f354233682a587714c2aabb244063
shin0112 <wnslcosltimo12@gmail.com>
2025-11-27 20:07:37 +0900

    fix: Gird 방식으로 시뮬레이션 -> world로 이동하도록 해서 절차적 맵 생성

--- Assets/01_Scripts/04_Dungeon/MapGenerator.cs
+++ Assets/01_Scripts/04_Dungeon/MapGenerator.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
-/// 스테이지 정보를 받고 맵을 자동으로 생성해주는 클래스
+/// Grid 기반 절차적 던전 생성기
 /// </summary>
 [System.Serializable]
 public class MapGenerator
@@ -12,8 +12,9 @@ public class MapGenerator
     private Dictionary<CorridorType, GameObject[]> _corridorsDict = new();
 
     // Room Generation Simulation
-    private RoomInfo[] _roomInfos;
-    private float[] _rotatioins = { 0, 90, 180, 270 };
+    private HashSet<Vector2Int> _occupied = new();           // 그리드
+    private List<RoomInfo> _rooms = new();
+    private int[] _rotatioins = { 0, 90, 180, 270 };
 
     public MapGenerator(CorridorSetData[] corridors)
     {
@@ -25,28 +26,17 @@ public class MapGenerator
 
     #region [public] 방 생성하기
     /// <summary>
-    /// [public] 스테이지 정보를 받고 방 랜덤 생성 후, 첫 번째 방을 스테이지의 루트로 설정
+    /// [public] 스테이지 정보를 받고 방 랜덤 생성 후, 루트 아래에 맵 생성
     /// </summary>
+    /// <param name="root"></param>
     /// <param name="stage"></param>
     /// <returns></returns>
     public GameObject Generate(Transform root, StageData stage)
     {
-        int count = stage.RoomCount;
-        _roomInfos = new RoomInfo[count];
+        _occupied.Clear();
+        _rooms.Clear();
 
-        bool success = false;
-        int attempts = 30;
-
-        while (!success && attempts-- > 0)
-        {
-            for (int i = 0; i < count; i++)
-            {
-                int random = Random.Range(0, stage.MapPrefabs.Length);
-                _roomInfos[i] = CreateRoomInfo(stage.MapPrefabs[random], random);
-            }
-
-            success = TrySimulateRooms(stage);  // 시뮬레이션
-        }
+        bool success = TrySimulate(stage);  // 시뮬레이션
 
         if (!success)
         {
@@ -60,22 +50,27 @@ public class MapGenerator
 
     #region 방 랜덤 배치 시뮬레이션
     /// <summary>
-    /// 시뮬레이션에 사용할 스트럭트
+    /// 시뮬레이션에 사용할 클래스
     /// </summary>
-    public struct RoomInfo
+    public class RoomInfo
     {
-        public int OriginalPrefabIndex;
+        public RoomData Data;
+        public Vector2Int GridPos;
+    }
 
-        public Vector3 Position;                // 월드 위치
-        public float RotationY;                 // 0/90/180/270
-
-        public Bounds LocalBounds;              // prefab 기준 bounds
-
-        public Vector3[] DoorLocalPositions;    // 문 로컬 위치 (배열)
-        public Vector3[] DoorLocalForwards;     // 문 로컬 forward (배열)
-
-        public bool[] DoorConnected;            // 문 사용되었는지 확인
-        public int ChosenDoor;                  // 선택된 문 인덱스
+    /// <summary>
+    /// prefab을 바탕으로 RoomInfo 생성하기
+    /// </summary>
+    /// <param name="prefab"></param>
+    /// <param name="index"></param>
+    /// <returns></returns>
+    private RoomInfo CreateRoomInfo(RoomData data)
+    {
+        return new RoomInfo()
+        {
+            Data = data,
+            GridPos = Vector2Int.zero
+        };
     }
 
     /// <summary>
@@ -83,17 +78,13 @@ public class MapGenerator
     /// </summary>
     /// <param name="stage"></param>
     /// <returns></returns>
-    private bool TrySimulateRooms(StageData stage)
+    private bool TrySimulate(StageData stage)
     {
-        for (int i = 0; i < stage.RoomCount; i++)
-        {
-            if (i == 0)
-            {
-                PlaceFirstRoom(ref _roomInfos[i]);
-                continue;
-            }
+        PlaceFirstRoom(stage.RoomPrefabs);
 
-            if (!TryPlaceNextRoom(i))
+        for (int i = 1; i < stage.RoomCount; i++)
+        {
+            if (!TryPlaceNextRoom(stage.RoomPrefabs))
             {
                 return false;
             }
@@ -103,153 +94,87 @@ public class MapGenerator
     }
 
     /// <summary>
-    /// prefab을 바탕으로 RoomInfo 생성하기
+    /// 첫 번째 방 마킹
     /// </summary>
-    /// <param name="prefab"></param>
-    /// <param name="index"></param>
-    /// <returns></returns>
-    private RoomInfo CreateRoomInfo(GameObject prefab, int index)
+    /// <param name="rooms"></param>
+    private void PlaceFirstRoom(RoomData[] rooms)
     {
-        RoomInfo info = new()
-        {
-            OriginalPrefabIndex = index,
-            LocalBounds = CalcLocalBounds(prefab)
+        RoomData rand = rooms.Random();
+
+        RoomInfo info = CreateRoomInfo(rand);
+
+        _rooms.Add(info);
+        MarkFootprint(info);
+    }
+
+    /// <summary>
+    /// 두 번째 방부터 Count 번째 방까지 마킹
+    /// </summary>
+    /// <param name="rooms"></param>
+    /// <returns></returns>
+    private bool TryPlaceNextRoom(RoomData[] rooms)
+    {
+        RoomData rand = rooms.Random();
+        RoomInfo prev = _rooms[_rooms.Count - 1];
+
+        Vector2Int[] dirs = {
+            new(1,0),
+            new (-1,0),
+            new (0,1),
+            new (0,-1),
         };
 
-        // Door
-        var connectors = prefab.GetComponentInChildren<RoomConnectors>();
-        int count = connectors.DoorPoints.Length;
+        dirs = dirs.Shuffle();
 
-        info.DoorLocalPositions = new Vector3[count];
-        info.DoorLocalForwards = new Vector3[count];
-        info.DoorConnected = new bool[count];
-
-        for (int i = 0; i < count; i++)
+        foreach (Vector2Int dir in dirs)
         {
-            Transform door = connectors.DoorPoints[i];
+            Vector2Int offset = dir * (prev.Data.Size.x + 5);
+            Vector2Int newPos = prev.GridPos + offset;
 
-            info.DoorLocalPositions[i] = door.localPosition;
-            Vector3 localForward = door.localRotation * Vector3.forward;
-            localForward.y = 0;
-            info.DoorLocalForwards[i] = localForward.normalized;
+            if (!IsFootprintOccupied(newPos, rand.Size))
+            {
+                RoomInfo info = new()
+                {
+                    Data = rand,
+                    GridPos = newPos
+                };
+
+                _rooms.Add(info);
+                MarkFootprint(info);
+                return true;
+            }
         }
 
-        return info;
+        return false;
+    }
+
+    private bool IsFootprintOccupied(Vector2Int pos, Vector2Int size)
+    {
+        for (int x = 0; x < size.x; x++)
+        {
+            for (int y = 0; y < size.y; y++)
+            {
+                Vector2Int cell = pos + new Vector2Int(x, y);
+                if (_occupied.Contains(cell)) { return true; }
+            }
+        }
+
+        return false;
     }
 
     /// <summary>
-    /// 첫 번째 방 배치
+    /// 
     /// </summary>
     /// <param name="info"></param>
-    private void PlaceFirstRoom(ref RoomInfo info)
+    private void MarkFootprint(RoomInfo info)
     {
-        info.Position = Vector3.zero;
-        info.RotationY = _rotatioins.Random();
-        info.ChosenDoor = 0;
-    }
-
-    /// <summary>
-    /// index 번째 방 배치
-    /// </summary>
-    /// <param name="index"></param>
-    /// <returns></returns>
-    private bool TryPlaceNextRoom(int index)
-    {
-        RoomInfo prev = _roomInfos[index - 1];
-        RoomInfo next = _roomInfos[index];
-
-        int attempts = 20;
-
-        while (attempts-- > 0)
+        for (int x = 0; x < info.Data.Size.x; x++)
         {
-            // 1) (index - 1) 번째 방에서 연결 가능한 문 탐색
-            List<int> availableDoors = new();
-            for (int i = 0; i < prev.DoorConnected.Length; i++)
+            for (int y = 0; y < info.Data.Size.y; y++)
             {
-                if (!prev.DoorConnected[i])
-                {
-                    availableDoors.Add(i);
-                }
-            }
-
-            if (availableDoors.Count == 0) { return false; }
-
-            int prevDoorIndex = availableDoors[Random.Range(0, availableDoors.Count)];
-
-            Vector3 prevDoorWPos = DoorWorldPos(prev, prevDoorIndex);       // 이전 방 문 위치 (World)
-            Vector3 prevDoorWDir = DoorWorldFoward(prev, prevDoorIndex);    // 이전 방 문 방향 (World)
-
-            // 2) 다음 방에서 사용할 문 선택
-            int nextDoorIndex = Random.Range(0, next.DoorLocalPositions.Length);
-            Vector3 nextDoorLDir = next.DoorLocalForwards[nextDoorIndex];   // 다음 방 문 방향 (Local)
-
-
-            // 3) 두 문이 마주보도록 회전 계산
-            Vector3 targetDir = -prevDoorWDir;                              // 타겟 방향 -> 이전 문 방향 반대
-
-            // - 목표 좌표와 로컬 좌표를 비교해서 회전해야 하는 각도 구하기
-            float targetY = Mathf.Atan2(targetDir.x, targetDir.z) * Mathf.Rad2Deg;
-            float localY = Mathf.Atan2(nextDoorLDir.x, nextDoorLDir.z) * Mathf.Rad2Deg;
-            float rotY = targetY - localY;
-            float snappedRotY = Mathf.Round(rotY / 90f) * 90f;              // 90도 단위로 회전하기 위한 스냅
-
-            Quaternion roomRot = Quaternion.Euler(0, snappedRotY, 0);       // 방 회전 목표 각도
-
-            // 4) 다음 방의 문 World 좌표 계산
-            float corridorLength = Random.Range(12f, 30f);
-            Vector3 nextDoorWPos =
-                prevDoorWPos + prevDoorWDir * corridorLength;               // 다음 방 문 위치 (World)
-
-            Vector3 nextDoorLPos = next.DoorLocalPositions[nextDoorIndex];  // 다음 방 문 위치 (Local)
-            Vector3 roomPos = nextDoorWPos - (roomRot * nextDoorLPos);
-            // - 통로 좌표의 y 위치(-2f) -> 밀리지 않게 room y 좌표 고정
-            roomPos.y = 0f;
-
-            // 5) 겹치지 않을 경우 사용
-            if (!IsOverlap(roomPos, roomRot, next.LocalBounds, index))
-            {
-                next.Position = roomPos;
-                next.RotationY = snappedRotY;
-                next.ChosenDoor = nextDoorIndex;
-
-                prev.DoorConnected[prevDoorIndex] = true;
-                next.DoorConnected[nextDoorIndex] = true;
-
-                // struct update
-                _roomInfos[index - 1] = prev;
-                _roomInfos[index] = next;
-
-                return true;
+                _occupied.Add(info.GridPos + new Vector2Int(x, y));
             }
         }
-
-        return false;
-    }
-
-    /// <summary>
-    /// index 번째 room 충돌 여부 확인
-    /// </summary>
-    /// <param name="pos"></param>
-    /// <param name="rot"></param>
-    /// <param name="local"></param>
-    /// <param name="index"></param>
-    /// <returns></returns>
-    private bool IsOverlap(Vector3 pos, Quaternion rot, Bounds local, int index)
-    {
-        Bounds bounds = local;
-        bounds.center = pos;
-
-        for (int i = 0; i < index; i++)
-        {
-            Bounds b = _roomInfos[i].LocalBounds;
-            b.center = _roomInfos[i].Position;
-            if (bounds.Intersects(b))
-            {
-                return true;
-            }
-        }
-
-        return false;
     }
     #endregion
 
@@ -263,33 +188,43 @@ public class MapGenerator
     {
         GameObject roomRoot = null;
 
-        for (int i = 0; i < _roomInfos.Length; i++)
+        for (int i = 0; i < _rooms.Count; i++)
         {
-            RoomInfo info = _roomInfos[i];
-            GameObject prefab = stage.MapPrefabs[info.OriginalPrefabIndex];
+            RoomInfo info = _rooms[i];
+            Vector3 pos = GridToWorld(info.GridPos);
 
-            GameObject room = Object.Instantiate(prefab, root);
-            room.transform.position = info.Position;
-            room.transform.rotation = Quaternion.Euler(0, info.RotationY, 0);
+            GameObject room = Object.Instantiate(
+                info.Data.Prefab,
+                pos,
+                Quaternion.identity,
+                root);
 
             if (i == 0)
             {
                 roomRoot = room;
             }
-        }
 
-        for (int i = 0; i < _roomInfos.Length - 1; i++)
-        {
-            var fromInfo = _roomInfos[i];
-            var toInfo = _roomInfos[i + 1];
+            if (i > 0)
+            {
+                Vector3 a = GridToWorld(_rooms[i - 1].GridPos);
+                Vector3 b = GridToWorld(_rooms[i].GridPos);
 
-            Vector3 from = DoorWorldPos(fromInfo, fromInfo.ChosenDoor);
-            Vector3 to = DoorWorldPos(toInfo, toInfo.ChosenDoor);
-
-            CreateCorridor(root, from, to);
+                CreateCorridor(root, a, b);
+                PlaceDoor(root, a, b);
+                PlaceDoor(root, b, a);
+            }
         }
 
         return roomRoot;
+    }
+
+    private void PlaceDoor(Transform root, Vector3 from, Vector3 to)
+    {
+        // 사용하고 싶은 doorPrefab으로 바꿔라
+        // 예시:
+        // Object.Instantiate(doorPrefab, position, rotation, root);
+
+        // 문 설치는 필요에 따라 구현.
     }
     #endregion
 
@@ -309,6 +244,7 @@ public class MapGenerator
         for (int i = 0; i < count; i++)
         {
             Vector3 pos = from + dir * (_corridorTileSize * i);
+            pos.y -= 2f;
 
             Object.Instantiate(
                 GetCorridorPrefab(CorridorType.Straight),
@@ -335,41 +271,14 @@ public class MapGenerator
     #endregion
 
     #region Helper
-
     /// <summary>
-    /// prefab의 로컬 Bounds 값 반환
+    /// Gird의 좌표를 World 좌표로 변환
     /// </summary>
-    /// <param name="prefab"></param>
+    /// <param name="cell"></param>
     /// <returns></returns>
-    private Bounds CalcLocalBounds(GameObject prefab)
+    Vector3 GridToWorld(Vector2Int cell)
     {
-        Renderer[] renderers = prefab.GetComponentsInChildren<Renderer>();
-        Bounds bounds = renderers[0].bounds;
-
-        foreach (var r in renderers)
-        {
-            bounds.Encapsulate(r.bounds);
-        }
-
-        bounds.center -= prefab.transform.position;     // local 기준 변경
-        return bounds;
-    }
-
-    private Vector3 DoorWorldPos(RoomInfo info, int index)
-    {
-        return info.Position +
-            Quaternion.Euler(0, info.RotationY, 0) * info.DoorLocalPositions[index];
-    }
-
-    /// <summary>
-    /// Room의 index 번째 문이 바라보는 좌표 구하기
-    /// </summary>
-    /// <param name="info"></param>
-    /// <param name="index"></param>
-    /// <returns></returns>
-    private Vector3 DoorWorldFoward(RoomInfo info, int index)
-    {
-        return Quaternion.Euler(0, info.RotationY, 0) * info.DoorLocalForwards[index];
+        return new Vector3(cell.x * Define.TileSize, 0, cell.y * Define.TileSize);
     }
     #endregion
 }
