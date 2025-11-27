f837f3493e642c30f514a7607c9ece8643e5b732
shin0112 <wnslcosltimo12@gmail.com>
2025-11-27 11:15:58 +0900

    feat: 던전 맵 레이어 상수로 관리

--- Assets/01_Scripts/01_Common/Define.cs
+++ Assets/01_Scripts/01_Common/Define.cs
@@ -1,10 +1,16 @@
+using UnityEngine;
+
 /// <summary>
 /// 게임에 필요한 상수를 정의합니다.
 /// </summary>
 public static class Define
 {
     #region 던전
+    // 몬스터
     public static int KillCountForMidBossSpawn = 100;
     public static float RespawnDelay = 10f;
+
+    // 던전 맵
+    public static LayerMask FloorLayer = 1 << 6;
     #endregion
 }
